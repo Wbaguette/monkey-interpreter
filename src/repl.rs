f867c8e0a4da48e2227bfb14cd97404bc133146e
jean-pierre <jeanpierre@derbes.net>
2023-08-02 07:34:04 -0400

    REPL is now a RPPL, next up: evaluating

--- src/repl.rs
+++ src/repl.rs
@@ -1,13 +1,19 @@
 #[allow(unused)]
 
 use std::io::{BufRead, Write};
+use color_eyre::owo_colors::OwoColorize;
+
 use crate::lexer::Lexer;
-use crate::lexer::token::{Token, TokenType};
+use crate::parser::Parser;
+use crate::parser::ast::{Program, Node};
+
+const PROMPT: &str = ">> ";
 
 pub fn start<R: BufRead, W: Write>(mut reader: R, mut writer: W) {
    loop {
-      print!(">> ");
-      Write::flush(&mut writer).expect("Failed to flush output");
+      write!(writer, "{}", PROMPT).expect("Failed to write prompt");
+      writer.flush().expect("Failed to flush output");
+
       let mut line: String = String::new();
       match reader.read_line(&mut line) {
          Ok(0) => continue,
@@ -17,16 +23,30 @@ pub fn start<R: BufRead, W: Write>(mut reader: R, mut writer: W) {
                continue;
             }
 
-            let mut lexer: Lexer = Lexer::new(trimmed_line.to_string());
-            loop {
-               let tok: Token = lexer.next_token().unwrap();
-               match tok.token_type {
-                  TokenType::EOF => break,
-                  _ => write!(writer, "{:?}\n", tok).expect("Failed to write Token"),
-               }
+            let lexer: Lexer = Lexer::new(trimmed_line.to_string());
+            let mut parser: Parser = Parser::new(lexer);
+
+            let program: Program = match parser.parse_program() {
+               Ok(p) => p,
+               Err(e) => panic!("{}", e),
+            };
+            if !parser.errors().is_empty() {
+               print_parser_errors(&mut writer, &parser.errors());
+               continue;
             }
+
+            write!(writer,"{}\n", program.string()).expect("Failed to write AST");
          },
          Err(e) => println!("Error reading input: {}", e),
       }
+   }
+}
+
+fn print_parser_errors<W: Write>(writer: &mut W, errors: &[String]) {
+   write!(writer, "{}", "Whoops!, We ran into some monkey business here.\n".bold().bright_red()).expect("Failed to write MONKEY_FACE");
+   write!(writer, "{}", " parser errors:\n".bold().bright_green()).expect("Failed to write parser errors clause");
+
+   for msg in errors {
+      write!(writer, "\t{}\n", msg).expect("Failed to write parser error")
    }
 }
